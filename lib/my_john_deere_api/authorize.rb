805935e6d39bb4f6d0669c906ccd65a058f57d60
Jaime Bellmyer <online@bellmyer.com>
2020-01-06 15:48:38 -0600

    add documentation for public methods

--- lib/my_john_deere_api/authorize.rb
+++ lib/my_john_deere_api/authorize.rb
@@ -11,7 +11,16 @@ class MyJohnDeereApi::Authorize
     environment: :production,
   }
 
-
+  ##
+  # Create an Authorize object using your John Deere API key/secret.
+  #
+  # Options:
+  #
+  # [:environment] *:production* or *:sandbox*, determines the base url
+  #                used to make oauth requests.
+  #
+  # [:base_url] set the base url directly, since in the future,
+  #             *environment* may affect other things.
 
   def initialize(api_key, api_secret, options={})
     options = DEFAULTS.merge(options)
@@ -22,6 +31,10 @@ class MyJohnDeereApi::Authorize
     @base_url = options[:base_url] || URLS[@environment]
   end
 
+  ##
+  # Option a url which may be used to obtain a verification
+  # code from the oauth server.
+
   def authorize_url
     return @authorize_url if defined?(@authorize_url)
 
@@ -31,6 +44,12 @@ class MyJohnDeereApi::Authorize
 
     @authorize_url = requester.authorize_url
   end
+
+  ##
+  # Turn a verification code into access tokens. If this is
+  # run from a separate process than the one that created
+  # the initial RequestToken, the request token/secret
+  # can be passed in.
 
   def verify(code, token=nil, secret=nil)
     token ||= request_token
@@ -43,13 +62,22 @@ class MyJohnDeereApi::Authorize
     nil
   end
 
+  ##
+  # oAuth Consumer which uses just the base url, for
+  # app-wide, non user-specific requests.
+
   def app_consumer
     @app_consumer ||= consumer(base_url)
   end
 
+  ##
+  # oAuth Consumer which uses the proper url for user-specific requests.
+
   def user_consumer
     @user_consumer ||= consumer("#{base_url}/platform")
   end
+
+  private
 
   def consumer(site)
     OAuth::Consumer.new(
@@ -63,8 +91,6 @@ class MyJohnDeereApi::Authorize
       authorize_url: links[:authorize_request_token]
     )
   end
-
-  private
 
   def links
     return @links if defined?(@links)
