f3f5bfcad69a7a902bb1fa7f63418be21ddb410f
Jaime Bellmyer <online@bellmyer.com>
2020-01-07 11:27:13 -0600

    extract all consumer logic from authorize class/tests

--- lib/my_john_deere_api/authorize.rb
+++ lib/my_john_deere_api/authorize.rb
@@ -1,34 +1,14 @@
 class MyJohnDeereApi::Authorize
-  attr_reader :api_key, :api_secret, :request_token, :request_secret, :access_token, :access_secret, :environment
-  attr_accessor :base_url
-
-  URLS = {
-    sandbox: 'https://sandboxapi.deere.com',
-    production: 'https://api.soa-proxy.deere.com',
-  }
-
-  DEFAULTS = {
-    environment: :production,
-  }
+  attr_reader :request_token, :request_secret, :access_token, :access_secret, :consumer
 
   ##
-  # Create an Authorize object using your John Deere API key/secret.
+  # Create an Authorize object.
   #
-  # Options:
-  #
-  # [:environment] *:production* or *:sandbox*, determines the base url
-  #                used to make oauth requests.
-  #
-  # [:base_url] set the base url directly, since in the future,
-  #             *environment* may affect other things.
+  # This is used to obtain authentication an access key/secret
+  # on behalf of a user.
 
-  def initialize(api_key, api_secret, options={})
-    options = DEFAULTS.merge(options)
-
-    @api_key = api_key
-    @api_secret = api_secret
-    @environment = options[:environment]
-    @base_url = options[:base_url] || URLS[@environment]
+  def initialize
+    @consumer = MyJohnDeereApi::Consumer.app_get
   end
 
   ##
@@ -38,7 +18,7 @@ class MyJohnDeereApi::Authorize
   def authorize_url
     return @authorize_url if defined?(@authorize_url)
 
-    requester = app_consumer.get_request_token
+    requester = consumer.get_request_token
     @request_token = requester.token
     @request_secret = requester.secret
 
@@ -55,72 +35,10 @@ class MyJohnDeereApi::Authorize
     token ||= request_token
     secret ||= request_secret
 
-    requester = OAuth::RequestToken.new(app_consumer, token, secret)
+    requester = OAuth::RequestToken.new(consumer, token, secret)
     access_object = requester.get_access_token(oauth_verifier: code)
     @access_token = access_object.token
     @access_secret = access_object.secret
     nil
-  end
-
-  ##
-  # oAuth Consumer which uses just the base url, for
-  # app-wide, non user-specific requests.
-
-  def app_consumer
-    @app_consumer ||= consumer(base_url)
-  end
-
-  ##
-  # oAuth Consumer which uses the proper url for user-specific requests.
-
-  def user_consumer
-    @user_consumer ||= consumer("#{base_url}/platform")
-  end
-
-  private
-
-  def consumer(site)
-    OAuth::Consumer.new(
-      api_key,
-      api_secret,
-      site: site,
-      header: header,
-      http_method: :get,
-      request_token_url: links[:request_token],
-      access_token_url: links[:access_token],
-      authorize_url: links[:authorize_request_token]
-    )
-  end
-
-  def links
-    return @links if defined?(@links)
-
-    catalog = OAuth::Consumer.new(api_key, api_secret)
-      .request(
-        :get,
-        "#{base_url}/platform/",
-        nil,
-        {},
-        header
-      ).body
-
-      @links = {}
-
-      JSON.parse(catalog)['links'].each do |link|
-        uri = URI.parse(link['uri'])
-        uri.query = nil
-
-        @links[keyify(link['rel'])] = uri.to_s
-      end
-
-      @links
-  end
-
-  def header
-    {accept: 'application/vnd.deere.axiom.v3+json'}
-  end
-
-  def keyify key_name
-    key_name.gsub(/^oauth/, '').gsub(/([a-z])([A-Z])/, '\1_\2').downcase.to_sym
   end
 end
--- test/lib/test_authorize.rb
+++ test/lib/test_authorize.rb
@@ -7,40 +7,29 @@ def contains_parameters?(uri)
 end
 
 def create_authorize
-  JD::Authorize.new(API_KEY, API_SECRET, environment: :sandbox)
+  VCR.use_cassette('catalog'){ JD::Consumer.send(:links) }
+  JD::Authorize.new
 end
 
 def fancy_url
   'https://example.com/turtles'
 end
 
-class AuthorizeTest < MiniTest::Test
+describe 'MyJohnDeereApi::Authorize' do
+  before do
+    JD::Consumer.config = {
+      api_key: API_KEY,
+      api_secret: API_SECRET,
+      environment: :sandbox
+    }
+  end
+
   describe 'initialization' do
-    it "accepts an API key and secret" do
-      authorize = JD::Authorize.new('key', 'secret')
+    it "sets the consumer to an app consumer with get requests" do
+      authorize = create_authorize
 
-      assert_equal 'key', authorize.api_key
-      assert_equal 'secret', authorize.api_secret
-    end
-
-    it 'defaults to production environment' do
-      authorize = JD::Authorize.new('key', 'secret')
-      assert_equal :production, authorize.environment
-    end
-
-    it 'defaults to production oauth url' do
-      authorize = JD::Authorize.new('key', 'secret')
-      assert_equal 'https://api.soa-proxy.deere.com', authorize.base_url
-    end
-
-    it "can set the environment" do
-      authorize = JD::Authorize.new('key', 'secret', environment: :sandbox)
-      assert_equal :sandbox, authorize.environment
-    end
-
-    it "can set the base_url via the environment" do
-      authorize = JD::Authorize.new('key', 'secret', environment: :sandbox)
-      assert_equal 'https://sandboxapi.deere.com', authorize.base_url
+      assert_kind_of OAuth::Consumer, authorize.consumer
+      assert_equal :get, authorize.consumer.http_method
     end
   end
 
@@ -49,8 +38,9 @@ class AuthorizeTest < MiniTest::Test
       authorize = create_authorize
 
       url = VCR.use_cassette('get_request_token') { authorize.authorize_url }
+      links = VCR.use_cassette('catalog') { JD::Consumer.send(:links) }
 
-      assert_includes url, "#{authorize.send(:links)[:authorize_request_token]}?oauth_token="
+      assert_includes url, "#{links[:authorize_request_token]}?oauth_token="
 
       query = URI.parse(url).query
       params = CGI::parse(query)
@@ -78,59 +68,6 @@ class AuthorizeTest < MiniTest::Test
 
       assert_match TOKEN_PATTERN, authorize.access_token
       assert_match SECRET_PATTERN, authorize.access_secret
-    end
-  end
-
-  describe '#links' do
-    it "returns a list of catalog urls" do
-      authorize = create_authorize
-
-      links = VCR.use_cassette("catalog"){ authorize.send(:links) }
-
-      assert_kind_of Hash, links
-
-      [:request_token, :authorize_request_token, :access_token].each do |link|
-        assert links.has_key?(link)
-        refute contains_parameters?(links[link])
-      end
-    end
-  end
-
-  describe '#base_url' do
-    it 'defaults to production deere url' do
-      authorize = JD::Authorize.new('key', 'secret')
-      assert_equal 'https://api.soa-proxy.deere.com', authorize.base_url
-    end
-
-    it 'can be set via accessor' do
-      authorize = create_authorize
-      authorize.base_url = fancy_url
-
-      assert_equal fancy_url, authorize.base_url
-    end
-  end
-
-  describe '#app_consumer' do
-    it 'creates a working oAuth consumer for non-user-specific requests' do
-      auth = create_authorize
-      app_consumer = VCR.use_cassette('app_consumer') { auth.app_consumer }
-
-      assert_kind_of OAuth::Consumer, app_consumer
-      assert_equal API_KEY, app_consumer.key
-      assert_equal API_SECRET, app_consumer.secret
-      assert_equal auth.base_url, app_consumer.site
-    end
-  end
-
-  describe '#user_consumer' do
-    it 'creates a working oAuth consumer for user-specific requests' do
-      auth = create_authorize
-      user_consumer = VCR.use_cassette('app_consumer') { auth.user_consumer }
-
-      assert_kind_of OAuth::Consumer, user_consumer
-      assert_equal API_KEY, user_consumer.key
-      assert_equal API_SECRET, user_consumer.secret
-      assert_equal "#{auth.base_url}/platform", user_consumer.site
     end
   end
 end
