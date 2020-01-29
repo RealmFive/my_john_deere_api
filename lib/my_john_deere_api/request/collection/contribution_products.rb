41eea8f389c08e265e45a23a7d31464aa511c247
Jaime Bellmyer <online@bellmyer.com>
2020-01-29 12:19:16 -0600

    round out contribution products collection

--- lib/my_john_deere_api/request/collection/contribution_products.rb
+++ lib/my_john_deere_api/request/collection/contribution_products.rb
@@ -2,33 +2,18 @@ require 'json'
 
 module MyJohnDeereApi::Request
   class Collection::ContributionProducts < Collection::Base
-  #   ##
-  #   # The resource path for the first page in the collection
-  #
-  #   def resource
-  #     "/organizations/#{associations[:organization]}/assets"
-  #   end
-  #
-  #   ##
-  #   # This is the class used to model the data
-  #
-  #   def model
-  #     MyJohnDeereApi::Model::Asset
-  #   end
-  #
-  #   ##
-  #   # Create a new asset
-  #
-  #   def create(attributes)
-  #     attributes.merge!(organization_id: associations[:organization])
-  #     Create::Asset.new(accessor, attributes).object
-  #   end
-  #
-  #   ##
-  #   # Retrieve an asset from JD
-  #
-  #   def find(asset_id)
-  #     Individual::Asset.new(accessor, asset_id).object
-  #   end
+    ##
+    # The resource path for the first page in the collection
+
+    def resource
+      "/contributionProducts?clientControlled=true"
+    end
+
+    ##
+    # This is the class used to model the data
+
+    def model
+      MyJohnDeereApi::Model::ContributionProduct
+    end
   end
 end
--- test/lib/my_john_deere_api/request/collection/contribution_products_test.rb
+++ test/lib/my_john_deere_api/request/collection/contribution_products_test.rb
@@ -3,6 +3,7 @@ require 'yaml'
 require 'json'
 
 describe 'MyJohnDeereApi::Request::Collection::ContributionProducts' do
+  let(:klass) { MyJohnDeereApi::Request::Collection::ContributionProducts }
   let(:organization_id) do
     contents = File.read('test/support/vcr/get_organizations.yml')
     body = YAML.load(contents)['http_interactions'].first['response']['body']['string']
@@ -11,110 +12,77 @@ describe 'MyJohnDeereApi::Request::Collection::ContributionProducts' do
 
   let(:client) { JD::Client.new(API_KEY, API_SECRET, environment: :sandbox, access: [ACCESS_TOKEN, ACCESS_SECRET]) }
   let(:accessor) { VCR.use_cassette('catalog') { client.send(:accessor) } }
-  let(:collection) { JD::Request::Collection::ContributionProducts.new(accessor, organization: organization_id) }
+  let(:collection) { klass.new(accessor, organization: organization_id) }
   let(:object) { collection }
 
   inherits_from JD::Request::Collection::Base
-  #
-  # describe '#initialize(access_token)' do
-  #   it 'accepts an access token' do
-  #     assert_kind_of OAuth::AccessToken, collection.accessor
-  #   end
-  #
-  #   it 'accepts associations' do
-  #     collection = JD::Request::Collection::Assets.new(accessor, organization: '123')
-  #
-  #     assert_kind_of Hash, collection.associations
-  #     assert_equal '123', collection.associations[:organization]
-  #   end
-  # end
-  #
-  # describe '#resource' do
-  #   it 'returns /organizations/{org_id}/assets' do
-  #     assert_equal "/organizations/#{organization_id}/assets", collection.resource
-  #   end
-  # end
-  #
-  # describe '#all' do
-  #   it 'returns all records' do
-  #     all = VCR.use_cassette('get_assets') { collection.all }
-  #
-  #     assert_kind_of Array, all
-  #     assert_equal collection.count, all.size
-  #
-  #     all.each do |item|
-  #       assert_kind_of JD::Model::Asset, item
-  #     end
-  #   end
-  # end
-  #
-  # describe '#create(attributes)' do
-  #   let(:title) { 'i like turtles' }
-  #   let(:category) { 'DEVICE' }
-  #   let(:type) { 'SENSOR' }
-  #   let(:subtype) { 'ENVIRONMENTAL' }
-  #
-  #   it 'creates a new asset with the given attributes' do
-  #     attributes = {
-  #       contribution_definition_id: ENV['CONTRIBUTION_DEFINITION_ID'],
-  #       title: title,
-  #       asset_category: category,
-  #       asset_type: type,
-  #       asset_sub_type: subtype
-  #     }
-  #
-  #     object = VCR.use_cassette('post_assets') { collection.create(attributes) }
-  #
-  #     assert_kind_of JD::Model::Asset, object
-  #     assert_equal title, object.title
-  #     assert_equal category, object.asset_category
-  #     assert_equal type, object.asset_type
-  #     assert_equal subtype, object.asset_sub_type
-  #   end
-  # end
-  #
-  # describe '#find(asset_id)' do
-  #   let(:asset_id) { '123' }
-  #
-  #   it 'retrieves the asset' do
-  #     asset = VCR.use_cassette('get_asset') { collection.find(asset_id) }
-  #     assert_kind_of JD::Model::Asset, asset
-  #   end
-  # end
-  #
-  # describe '#count' do
-  #   let(:server_response) do
-  #     contents = File.read('test/support/vcr/get_assets.yml')
-  #     body = YAML.load(contents)['http_interactions'].first['response']['body']['string']
-  #     JSON.parse(body)
-  #   end
-  #
-  #   let(:server_count) { server_response['total'] }
-  #
-  #   it 'returns the total count of records in the collection' do
-  #     count = VCR.use_cassette('get_assets') { collection.count }
-  #
-  #     assert_equal server_count, count
-  #   end
-  # end
-  #
-  # describe 'results' do
-  #   let(:asset_titles) do
-  #     contents = File.read('test/support/vcr/get_assets.yml')
-  #     body = YAML.load(contents)['http_interactions'].first['response']['body']['string']
-  #     JSON.parse(body)['values'].map{|v| v['title']}
-  #   end
-  #
-  #   it 'returns all records as a single enumerator' do
-  #     count = VCR.use_cassette('get_assets') { collection.count }
-  #     titles = VCR.use_cassette('get_assets') { collection.map(&:title) }
-  #
-  #     assert_kind_of Array, titles
-  #     assert_equal count, titles.size
-  #
-  #     asset_titles.each do |expected_title|
-  #       assert_includes titles, expected_title
-  #     end
-  #   end
-  # end
+
+  describe '#initialize(access_token)' do
+    it 'accepts an access token' do
+      assert_kind_of OAuth::AccessToken, collection.accessor
+    end
+
+    it 'accepts associations' do
+      collection = klass.new(accessor, organization: '123')
+
+      assert_kind_of Hash, collection.associations
+      assert_equal '123', collection.associations[:organization]
+    end
+  end
+
+  describe '#resource' do
+    it 'returns /contributionProducts' do
+      assert_equal "/contributionProducts?clientControlled=true", collection.resource
+    end
+  end
+
+  describe '#all' do
+    it 'returns all records' do
+      all = VCR.use_cassette('get_contribution_products') { collection.all }
+
+      assert_kind_of Array, all
+      assert_equal collection.count, all.size
+
+      all.each do |item|
+        assert_kind_of JD::Model::ContributionProduct, item
+      end
+    end
+  end
+
+  describe '#count' do
+    let(:server_response) do
+      contents = File.read('test/support/vcr/get_contribution_products.yml')
+      body = YAML.load(contents)['http_interactions'].first['response']['body']['string']
+      JSON.parse(body)
+    end
+
+    let(:server_count) { server_response['total'] }
+
+    it 'returns the total count of records in the collection' do
+      count = VCR.use_cassette('get_contribution_products') { collection.count }
+
+      assert_equal server_count, count
+    end
+  end
+
+  describe 'results' do
+    let(:product_names) do
+      contents = File.read('test/support/vcr/get_contribution_products.yml')
+      body = YAML.load(contents)['http_interactions'].first['response']['body']['string']
+
+      JSON.parse(body)['values'].map{|v| v['marketPlaceName']}
+    end
+
+    it 'returns all records as a single enumerator' do
+      count = VCR.use_cassette('get_contribution_products') { collection.count }
+      names = VCR.use_cassette('get_contribution_products') { collection.map(&:market_place_name) }
+
+      assert_kind_of Array, names
+      assert_equal count, names.size
+
+      product_names.each do |expected_name|
+        assert_includes names, expected_name
+      end
+    end
+  end
 end
